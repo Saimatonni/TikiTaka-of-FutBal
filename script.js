15540cd632db1db2e2562d8d357f7b5d4d04c846
saima <74361691+Saimatonni@users.noreply.github.com>
2022-05-18 17:26:02 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["Hello there", "Welcome to TikiTaka of Futbol"],
+    typeSpeed: 10,
+    backSpeed: 10,
+    loop: true
+});
+});
